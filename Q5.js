65c78ccb815bc3fa8f57edb8b17f94b0e094f553
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-07-14 03:36:35 -0700

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,6 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var name1 = ("famous_person =Albert Einstein");
+console.log("Albert Einstein");
+console.log("famous_person name");
+var greetings = ("A person who never made a mistake never tried anything new.");
+console.log("greetings");
---
+++ Q6.ts
@@ -0,0 +1,7 @@
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.var name=
+// task 5: Stripping Name
+var name3 = "\t Hamza \n"
+console.log("Name with white space:", name3)
+// stripping Name
+var stippedName = name3.trim()
+console.log("Name without white space:", stippedName)
